4a51d85992cde1d43f31a781f1897dbb16bc3331
musab <masab.2001by@gmail.com>
2022-11-22 20:33:39 +0300

    change variable name from x to dataRow

--- script.py
+++ script.py
@@ -4,12 +4,9 @@ files=[]
 filesFolser='Excel files'
 finalDF=pd.DataFrame()
 
-
 for i in range(10):
     df=pd.read_excel(f"{filesFolser}\\{i+1}-11-2022.xlsx")
     files.append({1:df,2:f"{i+1}-11-2022"})
-
-
 
 allBasicInfo=[]
 
@@ -18,18 +15,17 @@ for i in files:
 
 basicInfo=pd.concat(allBasicInfo)
 basicInfo.drop_duplicates(subset="Name",inplace=True)
-
  
 for n,e,g in zip(basicInfo["Name"],basicInfo["Email"],basicInfo["Guest"]) :
     sum=0
     absent=0
     dates={}
     for f in files:
-        x=f[1][f[1]["Name"] == n]
-        x=x.squeeze()
-        if(x.empty==False):
-            sum+=x["Time"]
-            dates[f[2]]=x["Time"]
+        dataRow=f[1][f[1]["Name"] == n]
+        dataRow=dataRow.squeeze()
+        if(dataRow.empty==False):
+            sum+=dataRow["Time"]
+            dates[f[2]]=dataRow["Time"]
 
         else:
             absent+=1
